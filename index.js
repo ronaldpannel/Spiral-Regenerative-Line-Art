d143b9f1cccd7c010cdbb6cc9a15e41bb5587703
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2023-02-21 10:40:33 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
-  canvas.width = 500;
-  canvas.height = 600;
+  canvas.width = 700;
+  canvas.height = 900;
 
   const linesArray = [];
   const numberOfLines = 50;
